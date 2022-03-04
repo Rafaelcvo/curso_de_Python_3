44ba6caee2782bc69d500980f8cca0b063adbbe6
Rafael Moreira <rafaelmoreiracvo@yahoo.com.br>
2022-03-04 08:59:16 -0300

    Aula 56 funcoes *args **kwargs.

--- secao_3/aula_52_def.py
+++ secao_3/aula_52_def.py
@@ -1,6 +1,15 @@
-
-def mensagem(nome='Rafael', msg='Olá'):
+def teste(nome='Rafael', msg='Olá'):
     print(msg, nome)
 
 
-mensagem('Joao', 'Bem Vindo')
+teste('Joao', 'Bem Vindo')
+
+
+def func(*args, **nomes):
+    print(args)
+    print(nomes)
+
+
+lista = [1,2,3,4,5]
+lista2 = [10,20,30]
+func(*lista, *lista2, nome='Luiz', sobrenome='Alves')
