6be7bdd337565e137e1e917f2a4951517c8a425f
Rafael Moreira <rafaelmoreiracvo@yahoo.com.br>
2022-03-04 10:02:39 -0300

    Aula 153 PyQT5.

---
+++ secao_6/aula_153_pyqt5.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication,QPushButton, QWidget, QGridLayout
+
+class App(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
+        self.btn = QPushButton('Texto do botao')
+        self.btn.setStyleSheet('font-size: 20px;')
+        self.btn.clicked.connect(self.acao)
+        self.grid.addWidget(self.btn, 0, 0, 1, 1)
+        self.setCentralWidget(self.cw)
+    def acao(self):
+        print('Teste')
+
+if __name__=='__main__':
+    qt = QApplication(sys.argv)
+    app = App()
+    app.show()
+    qt.exec_()
